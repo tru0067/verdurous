f55fcb2502e3b8ca33ce3ff93659439618f7a3bf
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2021-04-13 15:15:04 +1000

    Added light alacritty theme

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,15 @@
 
 mkdir -p ~/.config/nvim/colors
 if [[ ! -f ~/.config/nvim/colors/verdurous-dark.vim ]]; then
-    printf "source ~/verdurous/dark/vim.vim" > ~/.config/nvim/colors/verdurous-dark.vim
+    printf "source ~/verdurous/dark/vim.vim\n" > ~/.config/nvim/colors/verdurous-dark.vim
     printf "Adding skeleton ~/.config/nvim/colors/verdurous-dark.vim\n"
 fi
 mkdir -p ~/.config/alacritty
 if [[ ! -f ~/.config/alacritty/verdurous-dark.yml ]]; then
     printf "import:\n  - /home/tru0067/verdurous/dark/alacritty.yml\n" > ~/.config/alacritty/verdurous-dark.yml
     printf "Adding skeleton ~/.config/alacritty/verdurous-dark.yml\n"
+fi
+if [[ ! -f ~/.config/alacritty/verdurous-light.yml ]]; then
+    printf "import:\n  - /home/tru0067/verdurous/light/alacritty.yml\n" > ~/.config/alacritty/verdurous-light.yml
+    printf "Adding skeleton ~/.config/alacritty/verdurous-light.yml\n"
 fi
