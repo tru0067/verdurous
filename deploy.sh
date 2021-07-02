93bd75cb7cb98148147226e833974f635246d142
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2021-07-02 20:07:56 +1000

    Reorganize file structure

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@
 mkdir -p ~/.config/nvim/colors
 current_target=~/.config/nvim/colors/verdurous-dark.vim
 if [[ ! -f ${current_target} ]]; then
-    printf "source ${PWD}/dark/vim.vim\n" > ${current_target}
+    printf "source ${PWD}/themes/vim/verdurous-dark.vim\n" > ${current_target}
     printf "Creating skeleton ${current_target}\n"
 else
     printf "Skipping skeleton ${current_target}\n"
 fi
 current_target=~/.config/nvim/colors/verdurous-light.vim
 if [[ ! -f ${current_target} ]]; then
-    printf "source ${PWD}/light/vim.vim\n" > ${current_target}
+    printf "source ${PWD}/themes/vim/verdurous-light.vim\n" > ${current_target}
     printf "Creating skeleton ${current_target}\n"
 else
     printf "Skipping skeleton ${current_target}\n"
@@ -21,14 +21,14 @@ fi
 mkdir -p ~/.config/alacritty
 current_target=~/.config/alacritty/verdurous-dark.yml
 if [[ ! -f ${current_target} ]]; then
-    printf "import:\n  - ${PWD}/verdurous/dark/alacritty.yml\n" > ${current_target}
+    printf "import:\n  - ${PWD}/themes/alacritty/verdurous-dark.yml\n" > ${current_target}
     printf "Creating skeleton ${current_target}\n"
 else
     printf "Skipping skeleton ${current_target}\n"
 fi
 current_target=~/.config/alacritty/verdurous-light.yml
 if [[ ! -f ${current_target} ]]; then
-    printf "import:\n  - ${PWD}/verdurous/light/alacritty.yml\n" > ${current_target}
+    printf "import:\n  - ${PWD}/themes/alacritty/verdurous-light.yml\n" > ${current_target}
     printf "Creating skeleton ${current_target}\n"
 else
     printf "Skipping skeleton ${current_target}\n"
