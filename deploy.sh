4bbd03b6a19f9c24fa379e67f60e8bb9cbade24f
tru0067@TapLop <33011616+tru0067@users.noreply.github.com>
2021-07-02 21:52:43 +1000

    Remove vim light theme

--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,6 @@ if [[ ! -f ${current_target} ]]; then
 else
     printf "Skipping skeleton ${current_target}\n"
 fi
-current_target=~/.config/nvim/colors/verdurous-light.vim
-if [[ ! -f ${current_target} ]]; then
-    printf "source ${PWD}/themes/vim/verdurous-light.vim\n" > ${current_target}
-    printf "Creating skeleton ${current_target}\n"
-else
-    printf "Skipping skeleton ${current_target}\n"
-fi
 mkdir -p ~/.config/alacritty
 current_target=~/.config/alacritty/verdurous-dark.yml
 if [[ ! -f ${current_target} ]]; then
