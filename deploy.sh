63835d198c84d3ee70f36e205d52451cf9ebdd58
tru0067@CerPomLin <33011616+tru0067@users.noreply.github.com>
2021-07-13 19:55:27 +1000

    Reorder and add section comments

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,27 @@
 # This deploy script is fairly specialized for my usage. You may wish to edit it
 # for your own use.
 
-mkdir -p ~/.config/nvim/colors
-current_target=~/.config/nvim/colors/verdurous-dark.vim
-if [[ ! -f ${current_target} ]]; then
-    printf "source ${PWD}/themes/vim/verdurous-dark.vim\n" > ${current_target}
-    printf "Creating skeleton ${current_target}\n"
-else
-    printf "Skipping skeleton ${current_target}\n"
-fi
-mkdir -p ~/.config/alacritty
-current_target=~/.config/alacritty/verdurous-dark.yml
+# Alacritty.
+mkdir -p ~/.config/alacritty/colors
+current_target=~/.config/alacritty/colors/verdurous-dark.yml
 if [[ ! -f ${current_target} ]]; then
     printf "import:\n  - ${PWD}/themes/alacritty/verdurous-dark.yml\n" > ${current_target}
     printf "Creating skeleton ${current_target}\n"
 else
     printf "Skipping skeleton ${current_target}\n"
 fi
-current_target=~/.config/alacritty/verdurous-light.yml
+current_target=~/.config/alacritty/colors/verdurous-light.yml
 if [[ ! -f ${current_target} ]]; then
     printf "import:\n  - ${PWD}/themes/alacritty/verdurous-light.yml\n" > ${current_target}
+    printf "Creating skeleton ${current_target}\n"
+else
+    printf "Skipping skeleton ${current_target}\n"
+fi
+# Neovim.
+mkdir -p ~/.config/nvim/colors
+current_target=~/.config/nvim/colors/verdurous-darker.vim
+if [[ ! -f ${current_target} ]]; then
+    printf "source ${PWD}/themes/vim/verdurous-dark.vim\n" > ${current_target}
     printf "Creating skeleton ${current_target}\n"
 else
     printf "Skipping skeleton ${current_target}\n"
