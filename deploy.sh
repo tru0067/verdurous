474c8eb18271836a4ac4999d71835f4af47c2cf8
tru0067@CerPomLin <33011616+tru0067@users.noreply.github.com>
2021-07-13 20:09:08 +1000

    Add alacritty darker theme

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,13 @@ if [[ ! -f ${current_target} ]]; then
 else
     printf "Skipping skeleton ${current_target}\n"
 fi
+current_target=~/.config/alacritty/colors/verdurous-darker.yml
+if [[ ! -f ${current_target} ]]; then
+    printf "import:\n  - ${PWD}/themes/alacritty/verdurous-darker.yml\n" > ${current_target}
+    printf "Creating skeleton ${current_target}\n"
+else
+    printf "Skipping skeleton ${current_target}\n"
+fi
 current_target=~/.config/alacritty/colors/verdurous-light.yml
 if [[ ! -f ${current_target} ]]; then
     printf "import:\n  - ${PWD}/themes/alacritty/verdurous-light.yml\n" > ${current_target}
